fc57e5b90ac0a660125e49df477429bd43fda433
everdimension <everdimension@gmail.com>
2013-08-30 18:11:44 +0400

    Created git smth and added status update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '17d8a6ef3ce3b57847e1fd29d88023bdcc20b813ce3a32a57f2e4120b1c0c9cbeda61525b1a13af0386a5d75c646b1c73cc92b77239b1af9539682607fa49785'
